7f2e032f318913e9b5274b5c9af31302fec035a3
DESKTOP-13C4MS2\USER <l0533167552@gmail.com>
2025-03-23 02:47:49 +0200

    base

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { ApplayoutComponent } from '../Components/applayout/applayout.component';
+import { UsersListComponent } from '../Components/users-list/users-list.component';
+import { authGuard } from '../Guards/auth.guard';
+import { LoginComponent } from '../Components/login/login.component';
+import { DashboardComponent } from '../Components/dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '', component: ApplayoutComponent, canActivate: [authGuard], children: [
+            {path:'',component:DashboardComponent},
+            { path: 'users', component: UsersListComponent },
+            ]
+    },
+    {path:'login',component:LoginComponent},
+];
